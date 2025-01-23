8f42edfaa204a8e52c68e94d7502231eab1fbbfe
Logan Nelsen <loganjnelsen@gmail.com>
2025-01-23 14:57:20 -0600

    misc

--- app/components/sideNav.jsx
+++ app/components/sideNav.jsx
@@ -10,15 +10,19 @@ const SideNav = ({isScrolled}) => {
         setIsOpen(!isOpen);
     };
 
+    const handleClick = () => {
+        alert('Integration coming soon!');
+    };
+
     return (
         <div className="h-full">
             <button onClick={toggleSideNav} className={`hover:opacity-80 p-1 bg-gray-800 dark:bg-gray-200 dark:bg-opacity-20 text-xs bg-opacity-20 ${!isScrolled ? 'h-11 w-11' : 'h-8 w-8'} ${isOpen ? 'border border-white' : ''}`}>
                 {isOpen ? 'x' : '☰'}
             </button>
 
-            <div className={`side-nav fixed right-0 md:h-[100vh] w-[100vw] md:w-1/6 rounded-l md:bg-[linear-gradient(to_right,_rgba(105,105,105,0.3)_10%,_rgba(169,169,169,1)_100%)] text-white transition-transform transform ${isScrolled ? 'top-8' : 'top-11'} ${isOpen ? 'bg-[linear-gradient(to_top,_rgba(105,105,105,0.3)_10%,_rgba(169,169,169,1)_100%)] md:translate-x-0' : 'h-0 overflow-hidden md:translate-x-full'}`}>
+            <div className={`side-nav fixed right-0 md:h-[100vh] w-[100vw] md:w-1/6 rounded-l md:bg-[linear-gradient(to_right,_rgba(105,105,105,0.3)_10%,_rgba(169,169,169,1)_100%)] text-white transition-transform transform ${isScrolled ? 'top-8' : 'top-11'} ${isOpen ? 'bg-[linear-gradient(to_top,_rgba(105,105,105,0.8)_10%,_rgba(169,169,169,1)_100%)] md:translate-x-0' : 'h-0 overflow-hidden md:translate-x-full'}`}>
                 <ul className="p-4 space-y-4">
-                    <li><a href="#" className="block p-2 hover:bg-gray-700 rounded text-gray-400">Coming soon</a></li>
+                    <li><a href="#" onClick={handleClick} className="block p-2 hover:bg-gray-700 rounded">Gia API - predict data</a></li>
                     {/* <li><a href="#" className="block p-2 hover:bg-gray-700 rounded">Use Gia</a></li> */}
                 </ul>
             </div>
